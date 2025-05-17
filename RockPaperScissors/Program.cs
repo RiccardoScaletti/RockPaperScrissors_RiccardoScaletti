fa83284da7eb2f3ba1289f04c461f8a96c52cee4
Riccardo Scaletti <rickgamedeveloper@gmail.com>
2025-05-17 12:10:25 -0500

    title
    ascii art added

--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -37,6 +37,7 @@ namespace RockPaperScissors
             while ((playerWinCount < 5 && CpuWinCount < 5)) 
             {
                 //input management: Player
+                Console.WriteLine("__________                  __                         \r\n\\______   \\  ____    ____  |  | __                     \r\n |       _/ /  _ \\ _/ ___\\ |  |/ /                     \r\n |    |   \\(  <_> )\\  \\___ |    <                      \r\n |____|_  / \\____/  \\___  >|__|_ \\                     \r\n________\\/              \\/      \\/                     \r\n\\______   \\_____   ______    ____  _______             \r\n |     ___/\\__  \\  \\____ \\ _/ __ \\ \\_  __ \\            \r\n |    |     / __ \\_|  |_> >\\  ___/  |  | \\/            \r\n |____|    (____  /|   __/  \\___  > |__|               \r\n                \\/ |__|         \\/                     \r\n  _________        .__                                 \r\n /   _____/  ____  |__|  ______  ______  ____  _______ \r\n \\_____  \\ _/ ___\\ |  | /  ___/ /  ___/ /  _ \\ \\_  __ \\\r\n /        \\\\  \\___ |  | \\___ \\  \\___ \\ (  <_> ) |  | \\/\r\n/_______  / \\___  >|__|/____  >/____  > \\____/  |__|   \r\n        \\/      \\/          \\/      \\/                 ");
                 Console.WriteLine("insert input between: \n 1: Rock \n 2: Paper \n 3: Scissors \n 4: Lizard \n 5: Spock");
                 string playerInput;
                 int playerInputInt;
@@ -80,7 +81,6 @@ namespace RockPaperScissors
                 CpuChoice = (Choices)(RndChoiceCPU);
                 Console.WriteLine("Computer's Input : " + (Choices)RndChoiceCPU);
                   
-
                 void ComputerWins()
                 {
                     Console.WriteLine("\n Computer Wins");
